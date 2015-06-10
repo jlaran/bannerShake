d094f044745d7758bb63c88d02b11dc30e2b2b99
Juanci <juancarlos@Juans-MacBook-Pro.local>
2015-06-10 10:05:16 -0600

    update js

--- app/js/300x250_gngHand.js
+++ app/js/300x250_gngHand.js
@@ -3,19 +3,17 @@
 var banner = new Banner({
 	timelines: ["firstTimeline"],
 	elementsToRegister: [
-		{eventType: "click", element: "#banner", functionToCall: "changeAnimation"}
+		//{eventType: "click", element: "#banner", functionToCall: "changeAnimation"}
 	],
 	animations: {
 		firstFrame : function(){
-			customFunctions.initData();
-			timelinesArray[0].play();
+			//customFunctions.initData();
+			//timelinesArray[0].play();
 		}
 	},
 	timelinesAnimation: {
 		register: function(){
-			timelinesArray[0].to("#aspas", 0.4, {rotation:"+=90", ease: Power0.easeNone, onComplete: function(){
-				customFunctions.onRepeat();
-			}});
+			
 		}
 	}
 });
