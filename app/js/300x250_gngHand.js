8488f59cd8b2a08457f9bc1d0f38ecb9445af845
Rafael Aguilar <rafaguilar@gmail.com>
2015-06-11 09:50:11 -0600

    v.Hand images and frame2

--- app/js/300x250_gngHand.js
+++ app/js/300x250_gngHand.js
@@ -9,19 +9,27 @@ var banner = new Banner({
 		firstFrame : function(){
 			//customFunctions.initData();
 			timelinesArray[0].play();
+		},
+		//Aqui se ponen los frames para luego ejecutarlos en el onComplete
+		secondFrame : function(){
+			timelinesArray[1].play();
 		}
-
 	},
 	timelinesAnimation: {
 		register: function(){
-		timelinesArray[0].to("#footer", 0.3, {opacity:1})
+
+  		//Frame 1
+		timelinesArray[0].addLabel("frame1")
+						 .to("#footer", 0.3, {opacity:1})
 						 .to("#GNG_logo", 0.5, {opacity:1, left:5, ease:Power2.easeOut})
  						 .to(".blueButton", 0.5, {opacity:1, left:171, ease:Power2.easeOut}, "-=0.5" )
   						 .to("#ff-cta", 0.2, {opacity:1}, "-=0.1" )
    						 .to("#hand", 1, {top:83, left:110, ease:Power2.easeOut}) 
-  						 .to("#f1_txt1", 0.6, {opacity:1, onComplete:[1]});
-
-		timelinesArray[1].to("#hand", 0.2, {left:120, scaleY:0.5, ease:Power4.easeInOut})				 
+  						 .to("#f1_txt1", 0.6, {opacity:1, onComplete:function(){ animationsObject.secondFrame();
+  						 }});
+  		//Frame 2
+		timelinesArray[1].addLabel("frame2")
+						 .to("#hand", 0.3, {opacity:0});
 			}
 	}
 });
